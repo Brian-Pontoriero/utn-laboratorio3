144380c4dcd8589be6e25fbf2a2b7c178143f3c0
Brian Pontoriero <128325370+Brian-Pontoriero@users.noreply.github.com>
2023-07-17 03:29:10 -0300

    Delete database.php

--- app/database.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $dbname="beehkr99hkml66vltjbj";
-    $host="beehkr99hkml66vltjbj-mysql.services.clever-cloud.com";
-    $user ="uav7jlcswtvjsre2";
-    $password = "OpNTUfCa8bhSi2DAKwBm";
-    $respuesta_estado = "";
-?>
