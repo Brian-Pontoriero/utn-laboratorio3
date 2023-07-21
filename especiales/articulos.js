af7a961837469676a49cdeecbd1ffed5b03036ce
Brian Pontoriero <128325370+Brian-Pontoriero@users.noreply.github.com>
2023-07-21 20:07:59 -0300

    Add files via upload

---
+++ especiales/articulos.js
@@ -0,0 +1,44 @@
+var textoArticulos = '{' +
+    '"articulos": [' +
+      '{' +
+        '"codArt": "CAM01",' +
+        '"familia": "ROPA",' +
+        '"descripcion": "Camiseta estampada de algodón",' +
+        '"um": "TALLE",' +
+        '"fechaAlta": "20-05-2023",' +
+        '"saldoStock": 50' +
+      '},' +
+      '{' +
+        '"codArt": "PAN02",' +
+        '"familia": "ALIMENTOS",' +
+        '"descripcion": "Pan integral sin gluten",'+
+        '"um": "GR",'+
+        '"fechaAlta": "10-06-2023",'+
+        '"saldoStock": 100'+
+      '},'+
+      '{'+
+        '"codArt": "LIB03",'+
+        '"familia": "LIBROS",'+
+        '"descripcion": "Libro de ciencia ficción",'+
+        '"um": "EJEMPLAR",'+
+        '"fechaAlta": "05-04-2023",'+
+        '"saldoStock": 20'+
+      '},'+
+      '{'+
+        '"codArt": "ZAP04",'+
+        '"familia": "CALZADO",'+
+        '"descripcion": "Zapatos de cuero para hombre",'+
+       '"um": "NÚMERO",'+
+        '"fechaAlta": "15-07-2023",'+
+        '"saldoStock": 30'+
+      '},'+
+      '{'+
+        '"codArt": "CD05",'+
+        '"familia": "MÚSICA",'+
+        '"descripcion": "CD de música pop",'+
+        '"um": "UNIDAD",'+
+        '"fechaAlta": "01-08-2023",'+
+        '"saldoStock": 15'+
+      '}'+
+    ']'+
+  '}';
---
+++ especiales/familias.js
@@ -0,0 +1,25 @@
+var textoFamilias = '{'+
+    '"familias": ['+
+        '{'+
+        '"nombre": "ROPA",'+
+        '"descripcion": "Artículos de vestir"'+
+        '},'+
+        '{'+
+        '"nombre": "ALIMENTOS",'+
+        '"descripcion": "Productos comestibles"'+
+        '},'+
+        '{'+
+        '"nombre": "LIBROS",'+
+        '"descripcion": "Publicaciones impresas"'+
+        '},'+
+        '{'+
+        '"nombre": "CALZADO",'+
+        '"descripcion": "Zapatos y calzado"'+
+        '},'+
+        '{'+
+        '"nombre": "MÚSICA",'+
+        '"descripcion": "Grabaciones musicales"'+
+        '}'+
+    ']'+
+'}';
+  
